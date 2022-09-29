c7565a23a1f493606c99947b0651e13f5b73c51d
Agata Stawarz <agata.stawarz94@gmail.com>
2022-09-29 22:55:56 +0200

    Use Full Width Component and format displaying date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,63 @@
 import { NgIfContext } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, TemplateRef, ViewChild } from '@angular/core';
-import { Observable, timer } from "rxjs";
-import { map, shareReplay } from "rxjs/operators";
-import { differenceInSeconds } from "date-fns"
-import {  formatDuration, intervalToDuration } from 'date-fns'
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
+import { Observable, timer } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { differenceInSeconds } from 'date-fns';
+import { formatDuration, intervalToDuration } from 'date-fns';
 import { EventStorage } from './eventStorage';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
-
-export class AppComponent implements AfterViewInit {
-  @ViewChild('header') header: ElementRef<HTMLHeadingElement>;
-  @ViewChild('subheader') subheader: ElementRef<HTMLHeadingElement>;
-
+export class AppComponent {
   title = 'countdown-app';
   eventTime = '2022-10-26';
-  eventTitleFontSize: number = 8;
-  eventSubtitleFontSize: number = 12;
-  eventTitle: string
+  eventTitle: string;
   timeLeft$: Observable<Duration>;
   eventStorage: EventStorage;
-  
+
   constructor() {
     this.timeLeft$ = timer(0, 1000).pipe(
-      map(() => intervalToDuration({ start: Date.parse(this.eventTime), end: Date.now() }))
+      map(() =>
+        intervalToDuration({
+          start: Date.parse(this.eventTime),
+          end: Date.now(),
+        })
+      )
     );
     this.eventStorage = new EventStorage();
-    this.eventTitle = this.eventStorage.getData('eventTitle') || "Your event";
+    this.eventTitle = this.eventStorage.getData('eventTitle') || 'Your event';
   }
 
-  ngAfterViewInit() {
-    this.fitText(this.header.nativeElement);
+  formatTime({
+    years,
+    months,
+    days,
+    hours,
+    minutes,
+    seconds,
+  }: Duration): string {
+    let formattedTime = '';
+    formattedTime += this.formatTimePart('year', years);
+    formattedTime += this.formatTimePart('month', months);
+    formattedTime += this.formatTimePart('day', days);
+    formattedTime += `${hours}h, ${minutes}m, ${seconds}s`;
+
+    return formattedTime;
   }
 
-  onTitleChange(newTitle: string) {
-    this.eventTitle = newTitle;
-    setTimeout(() => this.fitText(this.header.nativeElement), 10);
-    this.eventStorage.saveData('eventTitle', newTitle)
-  }
-
-  private fitText(element: HTMLElement) {
-    const parentWidth = element.parentElement!.offsetWidth;
-    const elementWidth = element.offsetWidth;
-    this.eventTitleFontSize = this.eventTitleFontSize * (parentWidth / elementWidth);
+  private formatTimePart(name: string, value: number | undefined): string {
+    if (value) {
+      return `${value} ${name}${value > 1 ? 's' : ''}, `;
+    }
+    return '';
   }
 }
-
