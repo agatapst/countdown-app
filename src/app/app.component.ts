2e0e86a92365f426224e969a9e3093c6221e6f5d
Agata Stawarz <agata.stawarz94@gmail.com>
2022-09-29 15:58:54 +0200

    Add form with basic counting logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { NgIfContext } from '@angular/common';
+import { Component, TemplateRef } from '@angular/core';
+import { interval, Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
+import { differenceInSeconds } from "date-fns"
+import {  formatDuration, intervalToDuration } from 'date-fns'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'countdown-app';
+  eventTitle = "Your event"
+  eventTime = '2022-10-26'
+  
+  constructor() {
+    this.timeLeft$ = interval(1000).pipe(
+      map(x => formatDuration(intervalToDuration({ start: Date.parse(this.eventTime), end: Date.now() }))),
+      shareReplay(1)
+    );
+  }
+
+  public timeLeft$: any;
 }
