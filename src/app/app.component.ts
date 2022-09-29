b4707cc2af7bc88d4e5dd7ff3302dba655bb95c3
Agata Stawarz <agata.stawarz94@gmail.com>
2022-09-29 19:07:57 +0200

    Implement fitting font-size logic. Format displaying date.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { NgIfContext } from '@angular/common';
-import { Component, TemplateRef } from '@angular/core';
-import { interval, Observable } from "rxjs";
+import { AfterViewInit, Component, ElementRef, TemplateRef, ViewChild } from '@angular/core';
+import { Observable, timer } from "rxjs";
 import { map, shareReplay } from "rxjs/operators";
 import { differenceInSeconds } from "date-fns"
 import {  formatDuration, intervalToDuration } from 'date-fns'
@@ -10,17 +10,38 @@ import {  formatDuration, intervalToDuration } from 'date-fns'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+
+export class AppComponent implements AfterViewInit {
+  @ViewChild('header') header: ElementRef<HTMLHeadingElement>;
+  @ViewChild('subheader') subheader: ElementRef<HTMLHeadingElement>;
+
   title = 'countdown-app';
-  eventTitle = "Your event"
-  eventTime = '2022-10-26'
+  eventTitle = "Your event";
+  eventTime = '2022-10-26';
+  eventTitleFontSize: number = 10;
+  eventSubtitleFontSize: number = 8;
+  timeLeft$: Observable<Duration>;
   
   constructor() {
-    this.timeLeft$ = interval(1000).pipe(
-      map(x => formatDuration(intervalToDuration({ start: Date.parse(this.eventTime), end: Date.now() }))),
-      shareReplay(1)
+    this.timeLeft$ = timer(0, 1000).pipe(
+      map(() => intervalToDuration({ start: Date.parse(this.eventTime), end: Date.now() }))
     );
   }
 
-  public timeLeft$: any;
+  ngAfterViewInit() {
+    this.fitText(this.header.nativeElement);
+  }
+
+  onTitleChange(newTitle: string) {
+    this.eventTitle = newTitle;
+    setTimeout(() => this.fitText(this.header.nativeElement), 10);
+  }
+
+  private fitText(element: HTMLElement) {
+    const parentWidth = element.parentElement!.offsetWidth;
+    const elementWidth = element.offsetWidth;
+    this.eventTitleFontSize = this.eventTitleFontSize * (parentWidth / elementWidth);
+  }
 }
+
