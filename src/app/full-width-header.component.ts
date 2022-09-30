3a6a759ea964f7d301fac5e4a13fe760f57db505
Agata Stawarz <agata.stawarz94@gmail.com>
2022-09-30 19:57:38 +0200

    Polish styles

--- src/app/full-width-header.component.ts
+++ src/app/full-width-header.component.ts
@@ -11,9 +11,23 @@ import {
 @Component({
   selector: 'full-width-header',
   template: `
-    <h1 [ngStyle]="{ 'font-size': headerFontSize + 'vw' }" #header>
+    <h1
+      *ngIf="!subheader"
+      [ngStyle]="{ 'font-size': headerFontSize + 'vw' }"
+      #header
+    >
       {{ text }}
     </h1>
+    <h2
+      *ngIf="subheader"
+      [ngStyle]="{
+        'font-weight': 'normal',
+        'font-size': headerFontSize + 'vw'
+      }"
+      #header
+    >
+      {{ text }}
+    </h2>
   `,
   styles: [
     `
@@ -22,27 +36,32 @@ import {
         white-space: nowrap;
         width: 100%;
       }
-      h1 {
+      h1,
+      h2 {
         display: inline;
-        max-width: 100%;
       }
     `,
   ],
 })
 export class FullWidthHeaderComponent implements AfterViewInit, OnChanges {
   @Input() text: string;
+  @Input() subheader: boolean = false;
   @ViewChild('header') header: ElementRef<HTMLHeadingElement>;
 
   headerFontSize: number = 10;
 
   ngAfterViewInit(): void {
-    this.fitText(this.header.nativeElement);
+    this.scheduleResizeHeader();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (this.header && changes['text'].currentValue) {
-      setTimeout(() => this.fitText(this.header.nativeElement), 10);
+    if (changes['text'].currentValue) {
+      this.scheduleResizeHeader();
     }
+  }
+
+  private scheduleResizeHeader() {
+    setTimeout(() => this.fitText(this.header.nativeElement), 0);
   }
 
   private fitText(element: HTMLElement) {
